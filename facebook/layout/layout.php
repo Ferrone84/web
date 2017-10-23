609057a13be237010975ee555df035ae78dea008
Le Veve Mathieu <uapv1503202@pedago02a.univ-avignon.fr>
2017-10-23 17:20:44 +0200

    temporaire

--- facebook/layout/layout.php
+++ facebook/layout/layout.php
@@ -1,17 +1,17 @@
 <!DOCTYPE html>
 <html lang="fr">
-<head>
-	<meta charset="utf-8">
-	<title>Facebook</title>
-	<link rel="stylesheet" type="text/css" href="css/style.css">
-	<script type="text/javascript" src="js/jquery.js"></script>
-	<script type="text/javascript" src="js/app.js"></script>
-   
-</head>
-<body>
-	<!-- Zone de notification -->
-	<div id="notif" class="center"><?=$context->notif?></div>
-	<div id="view"><?php include($template_view); ?></div>
-    
-</body>
+	<head>
+		<meta charset="utf-8">
+		<title>Facebook</title>
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<script type="text/javascript" src="js/jquery.js"></script>
+		<script type="text/javascript" src="js/app.js"></script>
+	   
+	</head>
+	<body>
+		<!-- Zone de notification -->
+		<div id="notif" class="center"><?=$context->notif?></div>
+		<div id="view"><?php include($template_view); ?></div>
+	    
+	</body>
 </html>
