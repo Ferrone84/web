10429af2dbfa17fabcb0671d2762ce5b893d600a
Mathieu <levevemathieu@gmail.com>
2017-11-09 22:51:09 +0100

    TP3 review part LM
    add file : sendMessageSuccess (view)
    
    CHANGES:
    SendMessageSuccess -view-
    
    FILES:
    css et html

--- facebook/view/sendMessageSuccess.php
+++ facebook/view/sendMessageSuccess.php
@@ -1,14 +1,19 @@
-<div class="col-xs-12 form-send-message">
-    <form action="profil" method="post" class="form-control"
-        <div class = "text-area"
-            <textarea rows="4" cols="50">
-                This is a sample Text This is a sample Text This is a sample Text This is a sample Text This is a sample Text This is a sample Text This is a sample Text
-                This is a sample Text This is a sample Text This is a sample Text This is a sample Text This is a sample Text This is a sample Text
-                This is a sample Text This is a sample Text This is a sample Text This is a sample Text This is a sample Text
-            </textarea>
-        </div>
-        <div class="btn-submit">
-            <button type="submit">Envoyer votre message</button>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="../../css/style.css">
+</head>
+<body>
+<div class="col-sm-6 form-send-message">
+    <form action="profil" method="post" class="form-control">
+        <div>
+            <textarea class="text-area" cols="50" rows="20" placeholder="Ecrivez un message"></textarea>
+            <div class="div-btn-submit col-sm-6">
+                <div class="btn-submit col-sm-6">
+                    <button type="submit"><span class="btn-label-submit">Envoyer votre message</span></button>
+                </div>
+            </div>
         </div>
     </form>
 </div>
+</body>
+</html>
