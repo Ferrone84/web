8d97bac9bdcb078f6d3a71e3c7ae16de1bca7ce7
Ferrone84 <nico.duret@orange.fr>
2017-11-10 13:48:46 +0100

    manque 2/3 conneries sur le chat

--- js/app.js
+++ js/app.js
@@ -5,20 +5,28 @@ $(document).ready(function(){
 		logout();
 	});
 
-	$("#chat").resizable({ alsoResize: "#chats" }).draggable({ cursor: "move", handle: "#chat_toolbar" });
+	$("#chat").resizable({ alsoResize: "#chats,#chat_form" }).on('resize', function(e) {
+		e.stopPropagation();
+	});
+	$("#chat").draggable({ cursor: "move", handle: "#chat_toolbar" });
 
-	$("#reduce").on('click', function(event) {
-		$("#notif").html("<span class=\"success\">Vous vous êtes bien déconnecté.</span>");
+	$("#reduce").on('click', function() {
+		saveHeight = $("#chat").height()+2;
 		$("#chats").css('display', 'none');
 		$("#chat_form").css('display', 'none');
-		$(this).replaceWith("<span id=\"maximize\" class=\"glyphicon glyphicon-resize-full\" style=\"cursor: pointer\"></span>");
+		$(this).hide();
+		$("#maximize").show();
+		$("#chat").css('height', '24');
+		$("#chat").resizable('disable');
 	});
 
-	$("#maximize").on('click', function(event) {
-		$("#notif").html("<span class=\"success\">Vous vous êtes bien cac.</span>");
+	$("#maximize").on('click', function() {
 		$("#chats").css('display', 'block');
 		$("#chat_form").css('display', 'block');
-		$(this).replaceWith("<span id=\"reduce\" class=\"glyphicon glyphicon-resize-small\" style=\"cursor: pointer\"></span>");
+		$(this).hide();
+		$("#reduce").show();
+		$("#chat").css('height', saveHeight);
+		$("#chat").resizable('enable');
 	});
 });
 
@@ -40,4 +48,8 @@ function logout() {
 	});
 }
 
-/*https://codepen.io/kompiajaib/pen/rOZyWQ*/
+/*
+https://www.google.fr/search?client=ubuntu&hs=4UV&channel=fs&dcr=0&biw=1375&bih=769&ei=YvQBWvXVGYvca9n0iugF&q=jquery+ui+resizable+also+resize+window&oq=jquery+ui+resizable+also+resize+window&gs_l=psy-ab.3..33i22i29i30k1.718508.720857.0.720977.13.13.0.0.0.0.142.1040.9j2.11.0....0...1.1.64.psy-ab..2.11.1034...35i39k1.0.GdavuEmy4yI
+https://stackoverflow.com/questions/7494378/jquery-ui-resizable-fire-window-resize-event
+https://stackoverflow.com/questions/23473105/jquery-ui-resize-also-triggers-a-resize-on-window
+*/
